60942a8eb7049be11040dda15a1689681671ce53
aidai1233 <samatovaajdaj@gmail.com>
2024-09-23 22:07:38 +0600

    HomeWork№3

--- blog/urls.py
+++ blog/urls.py
@@ -17,12 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from posts.views import text_view, html_view, list_view, detail_view
-
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('text/', text_view),
     path('', html_view),
     path('posts/', list_view),
     path('posts/<int:post_id>/', detail_view)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/models.py
+++ posts/models.py
@@ -1,16 +1,36 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
 class Post(models.Model):
+    image = models.ImageField(blank=True, null=True, upload_to='posts')
     title = models.CharField(max_length=100)
-    content = models.TextField()
+    content = models.TextField(blank=True, null=True)
     rate = models.IntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True, related_name='posts')
+    tags = models.ManyToManyField(Tag,null=True, blank=True)
 
     def __str__(self):
         return f"{self.title}"
 
 
+class Comment(models.Model):
+    text = models.CharField(max_length=400)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
 
